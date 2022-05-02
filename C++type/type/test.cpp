95ab218fdb54333d2b25d6596e37ccd741463cc0
Qin-63 <2805436131@qq.com>
2022-05-02 11:52:55 +0800

    C++ Type

---
+++ C++type/type/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  
+#include <limits>
+#include <windows.h>
+
+using namespace std;  
+  
+int main()  
+{  
+    cout << "type: \t\t" << "************size**************"<< endl;  
+    cout << "bool: \t\t" << "所占字节数" << sizeof(bool);   
+    return 0;  
+}
